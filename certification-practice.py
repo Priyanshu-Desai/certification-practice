e1d5df8852acb85b161b7063977e118fd39388cb
Priyanshu-Desai <desai09p@gmail.com>
2023-03-14 16:35:30 +0000

    added code to display question

--- certification-practice.py
+++ certification-practice.py
@@ -1,5 +1,6 @@
+#emergency debugger
+# import pdb; pdb.set_trace()
 #importing modules
-#import pdb; pdb.set_trace()
 from random import choice
 import json
 from tkinter import *
@@ -27,6 +28,10 @@ def choose_certification():
     chosenCertification = certifications.get()
     clear_frame()
 
+#defining function to get the value of selected checkbuttons in question
+def submit_question():
+    global chosenAnswers
+
 # creating a 500 by 500 window
 frame = ttk.Frame(root, width=500, height=500)
 frame.pack()
@@ -52,18 +57,60 @@ nextbutton = ttk.Button(root, text='Next -->', command=choose_certification)
 nextbutton.pack()
 nextbutton.place(relx=0.9, rely=0.9, anchor='center')
 
-#givequestions to user on certification
+#give questions to user on certification
 nextbutton.wait_variable(waitstate)
+#while loop for infinite questions
 loop = True
 while loop == True:
+    #checking which certification the user has chosen
     if chosenCertification == 'az900':
+        #randomly selecting a question
         selectedQuestion = choice(az900)
-        
+
+    #reading question details and saving them to a variable
     questionText = selectedQuestion['question']
     optionAText = selectedQuestion['options']['A']
     optionBText = selectedQuestion['options']['B']
     optionCText = selectedQuestion['options']['C']
     optionDText = selectedQuestion['options']['D']
+
+    #setting control variables to capture state of checkbox
+    optionA = BooleanVar()
+    optionB = BooleanVar()
+    optionC = BooleanVar()
+    optionD = BooleanVar()
+
+    #creating taxt label
+    question = ttk.Label(root, text=questionText)
+    question.pack()
+    question.place(relx=0.5, rely=0.1, anchor='n')
+
+    #creating cherckboxes if the value of the option is not None
+    print(optionAText)
+    print(optionBText)
+    print(optionCText)
+    print(optionDText)
+    if optionAText != None:
+        optionA = ttk.Checkbutton(root, text=optionAText, variable=optionA)
+        optionA.pack()
+        optionA.place(relx='0.5', rely='0.3', anchor='center')
+    if optionBText != None:
+        optionB = ttk.Checkbutton(root, text=optionBText, variable=optionB)
+        optionB.pack()
+        optionB.place(relx='0.5', rely='0.4', anchor='center')
+    if optionCText != None:
+        optionC = ttk.Checkbutton(root, text=optionCText, variable=optionC)
+        optionC.pack()
+        optionC.place(relx='0.5', rely='0.5', anchor='center')
+    if optionDText != None:
+        optionD = ttk.Checkbutton(root, text=optionDText, variable=optionD)
+        optionD.pack()
+        optionD.place(relx='0.5', rely='0.6', anchor='center')
+
+    #creating submit Button
+
+
+    #exiting loop
     loop = False
 
 #execute tkinter
